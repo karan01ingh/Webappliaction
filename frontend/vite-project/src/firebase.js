43896a14e112ce768c96dcd6091ac05ea11554f3
Karan01ingh <Singhkunal05046@gmail.com>
2024-08-26 15:25:22 +0530

    donw with the firebase setting for my project

---
+++ frontend/vite-project/src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAoUr_7PtV3kki4iHMcFGz5lpspicte1CQ",
+    authDomain: "ecommerce-2ab94.firebaseapp.com",
+    projectId: "ecommerce-2ab94",
+    storageBucket: "ecommerce-2ab94.appspot.com",
+    messagingSenderId: "919995772348",
+    appId: "1:919995772348:web:48e7d60295c14d115b8fb9",
+    measurementId: "G-X1KN6NLHJE"
+  };
