dc234f5093888df00d85653f2b95a3428736a904
Karan01ingh <Singhkunal05046@gmail.com>
2024-08-29 02:22:18 +0530

    completed with the responsive design of the home page

--- frontend/vite-project/src/pages/Home.jsx
+++ frontend/vite-project/src/pages/Home.jsx
@@ -2,22 +2,131 @@ import React from 'react'
 
 function Home() {
   return (
-    <div>
+    <div className='bg-slate-200'>
       <img className='w-full h-auto -z-10 -mb-20 gradient-mask-b-[transparent,rgba(0,0,0,2.0)_0px,rgba(0,0,0,0.1)_90%] ' src="https://imgs.search.brave.com/RMEB-mtDPpKilEt8gGk90ZsGjNQw8IILzNz9EzRR7jI/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pbWFn/ZXMtbmEuc3NsLWlt/YWdlcy1hbWF6b24u/Y29tL2ltYWdlcy9H/LzE1L2RpZ2l0YWwv/dmlkZW8vbWVyY2gv/T3RoZXIvQlJORF9N/VEgyMV9TQURMUERl/c2t0b3BfMTQ1M3gz/NjNfRmluYWxfbm9s/b2NhbGVfUFZENzQz/Nl9DYW5hZGEuanBn" alt="" />
-      <div className='w-full h-screen'>
-        <div className='w-full h-1/2 bg-orange-700 flex flex-row p-2 justify-around '>
-             <div className='w-1/2 h-full bg-indigo-700'></div>
-             <div className='w-1/4 h-full bg-indigo-700'></div>
-        </div>
-        <div className='w-full h-1/2 bg-orange-700  flex flex-row p-2 justify-around'>
-            <div className='w-1/4 h-full bg-indigo-700'></div>
-            <div className='w-1/4 h-full bg-indigo-700'></div>
-            <div className='w-1/4 h-full bg-indigo-700'></div>
-        </div>
-        <div className='w-full h-1/2 bg-orange-700 flex flex-row p-2 justify-around '>
-              <div className='w-full h-full bg-indigo-700'></div>
-        </div>
+      <div className='w-full h-auto mt-16 flex flex-col'>
+        <div className='w-full h-44 flex flex-row p-1 sm:h-64  lg:h-80'>
+            <div className='w-1/3 h-full border-1 border-black flex flex-col text-left bg-white m-1 text-xs p-1 md:text-base  '>
+                <div className='h-1/3 flex flex-col '>
+                    <div className=''>Sports MAterial </div>
+  
+                    <div className='font-bold'>price</div>
 
+                    <div>star</div>
+                </div>
+       
+                <div className='h-2/3 w-full flex flex-col justify-between overflow-hidden'>
+                    <div className='h-2/3 '><img className='object-cover' src="https://imgs.search.brave.com/i-YzE_gDJglAQCCGfNNG08VZBvgZLoeo2PNiolLm6-U/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9keWwz/NDdoaXd2M2N0LmNs/b3VkZnJvbnQubmV0/L2FwcC91cGxvYWRz/LzIwMjMvMDkvU3Rh/ZGl1bV9WMl8xOTgw/JUUyJTgwJThBJUMz/JTk3JUUyJTgwJThB/MTEyOF9UZXh0LXNj/YWxlZC5qcGc" alt=""  /></div>
+                    <div className='h-auto w-full flex flex-row justify-center pb-1'>
+                        <button className='bg-yellow-500  rounded-sm p-1 w-22'>Add to cart</button>
+                    </div>
+                  
+
+                </div>
+        </div>
+        {/* 1st */}
+        <div className='w-1/3 h-full border-1 border-black flex flex-col text-left bg-white m-1 text-xs p-1 md:text-base' >
+        <div className='h-1/3 flex flex-col '>
+                    <div className=''>Sports MAterial </div>
+  
+                    <div className='font-bold'>price</div>
+
+                    <div>star</div>
+                </div>
+       
+                <div className='h-2/3 w-full flex flex-col justify-between overflow-hidden'>
+                    <div className='h-2/3 '><img className='object-contain' src="https://imgs.search.brave.com/i-YzE_gDJglAQCCGfNNG08VZBvgZLoeo2PNiolLm6-U/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9keWwz/NDdoaXd2M2N0LmNs/b3VkZnJvbnQubmV0/L2FwcC91cGxvYWRz/LzIwMjMvMDkvU3Rh/ZGl1bV9WMl8xOTgw/JUUyJTgwJThBJUMz/JTk3JUUyJTgwJThB/MTEyOF9UZXh0LXNj/YWxlZC5qcGc" alt=""  /></div>
+                    <div className='h-auto w-full flex flex-row justify-center pb-1'>
+                        <button className='bg-yellow-500  rounded-sm p-1 w-22'>Add to cart</button>
+                    </div>
+                  
+
+                </div>
+        </div>
+        {/* 2nd */}
+        <div className='w-1/3 h-full border-1 border-black flex flex-col text-left bg-white m-1 text-xs p-1 md:text-base'>
+        <div className='h-1/3 flex flex-col '>
+                    <div className=''>Sports MAterial </div>
+  
+                    <div className='font-bold'>price</div>
+
+                    <div>star</div>
+                </div>
+       
+                <div className='h-2/3 w-full flex flex-col justify-between overflow-hidden'>
+                    <div className='h-2/3 '><img className='object-contain' src="https://imgs.search.brave.com/i-YzE_gDJglAQCCGfNNG08VZBvgZLoeo2PNiolLm6-U/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9keWwz/NDdoaXd2M2N0LmNs/b3VkZnJvbnQubmV0/L2FwcC91cGxvYWRz/LzIwMjMvMDkvU3Rh/ZGl1bV9WMl8xOTgw/JUUyJTgwJThBJUMz/JTk3JUUyJTgwJThB/MTEyOF9UZXh0LXNj/YWxlZC5qcGc" alt=""  /></div>
+                    <div className='h-auto w-full flex flex-row justify-center pb-1'>
+                        <button className='bg-yellow-500  rounded-sm p-1 w-22'>Add to cart</button>
+                    </div>
+                  
+
+                </div>
+        </div>
+        {/* 3rd */}
+        </div>
+        {/* new row */}
+        <div className='w-full h-44 flex flex-row p-1 sm:h-64  lg:h-80'>
+        <div className='w-1/2 h-full border-1 border-black flex flex-col text-left bg-white m-1 text-xs p-1 md:text-base'>
+              
+        <div className='h-1/3 flex flex-col '>
+                    <div className=''>Sports MAterial </div>
+  
+                    <div className='font-bold'>price</div>
+
+                    <div>star</div>
+                </div>
+       
+                <div className='h-2/3 w-full flex flex-col justify-between overflow-hidden'>
+                    <div className='h-2/3 '><img className='object-contain' src="https://imgs.search.brave.com/i-YzE_gDJglAQCCGfNNG08VZBvgZLoeo2PNiolLm6-U/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9keWwz/NDdoaXd2M2N0LmNs/b3VkZnJvbnQubmV0/L2FwcC91cGxvYWRz/LzIwMjMvMDkvU3Rh/ZGl1bV9WMl8xOTgw/JUUyJTgwJThBJUMz/JTk3JUUyJTgwJThB/MTEyOF9UZXh0LXNj/YWxlZC5qcGc" alt=""  /></div>
+                    <div className='h-auto w-full flex flex-row justify-center pb-1'>
+                        <button className='bg-yellow-500  rounded-sm p-1 w-22'>Add to cart</button>
+                    </div>
+                  
+
+                </div>
+        </div>
+        {/* <div className='w-1/3 h-full bg-cyan-600 m-1'>2</div> */}
+        <div className='w-1/2 h-full border-1 border-black flex flex-col text-left bg-white m-1 text-xs p-1 md:text-base'>
+        <div className='h-1/3 flex flex-col '>
+                    <div className=''>Sports MAterial </div>
+  
+                    <div className='font-bold'>price</div>
+
+                    <div>star</div>
+                </div>
+       
+                <div className='h-2/3 w-full flex flex-col justify-between overflow-hidden'>
+                    <div className='h-2/3 '><img className='object-contain' src="https://imgs.search.brave.com/i-YzE_gDJglAQCCGfNNG08VZBvgZLoeo2PNiolLm6-U/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9keWwz/NDdoaXd2M2N0LmNs/b3VkZnJvbnQubmV0/L2FwcC91cGxvYWRz/LzIwMjMvMDkvU3Rh/ZGl1bV9WMl8xOTgw/JUUyJTgwJThBJUMz/JTk3JUUyJTgwJThB/MTEyOF9UZXh0LXNj/YWxlZC5qcGc" alt=""  /></div>
+                    <div className='h-auto w-full flex flex-row justify-center pb-1'>
+                        <button className='bg-yellow-500  rounded-sm p-1 w-22'>Add to cart</button>
+                    </div>
+                  
+
+                </div>
+        </div>
+        </div>
+        <div className='w-full h-44 flex flex-row p-1 sm:h-64  lg:h-80'>
+        {/* <div className='w-1/3 h-full bg-cyan-600 m-1'>1</div>
+        <div className='w-1/3 h-full bg-cyan-600 m-1'>2</div> */}
+        <div className='w-full h-full border-1 border-black flex flex-col text-left bg-white m-1 text-xs p-1 md:text-base'>
+        <div className='h-1/3 flex flex-col '>
+                    <div className=''>Sports MAterial </div>
+  
+                    <div className='font-bold'>price</div>
+
+                    <div>star</div>
+                </div>
+       
+                <div className='h-2/3 w-full flex flex-col justify-between overflow-hidden'>
+                    <div className='h-2/3 '><img className='object-contain' src="https://imgs.search.brave.com/i-YzE_gDJglAQCCGfNNG08VZBvgZLoeo2PNiolLm6-U/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9keWwz/NDdoaXd2M2N0LmNs/b3VkZnJvbnQubmV0/L2FwcC91cGxvYWRz/LzIwMjMvMDkvU3Rh/ZGl1bV9WMl8xOTgw/JUUyJTgwJThBJUMz/JTk3JUUyJTgwJThB/MTEyOF9UZXh0LXNj/YWxlZC5qcGc" alt=""  /></div>
+                    <div className='h-auto w-full flex flex-row justify-center pb-1'>
+                        <button className='bg-yellow-500  rounded-sm p-1 w-22'>Add to cart</button>
+                    </div>
+                  
+
+                </div>
+        </div>
+        </div>
       </div>
     </div>
   )
